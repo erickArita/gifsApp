24dbd434fc7612a76ecc7e1308e2080b888ce5e8
erickArita <erick.emao@gmail.com>
2020-08-27 19:43:09 -0600

    docs added

---
+++ docs/precache-manifest.83a176638cada10e2654c39e2f681d69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7919ddc562f5790857202a019c5f27f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53870fe4b78a1450281b",
+    "url": "/static/css/main.4dd0028e.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53870fe4b78a1450281b",
+    "url": "/static/js/main.02e02c6b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
